7a6a2aed7ff8eb61a1232bc6104cba04cf381274
Rahul choudhary <heyitsmechoudhary@gmail.com>
2025-05-06 13:07:07 +0530

    Initial Commit

---
+++ iNote Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iNote Watch App
+//
+//  Created by Rahul choudhary on 06/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iNote Watch App/iNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNoteApp.swift
+//  iNote Watch App
+//
+//  Created by Rahul choudhary on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct iNote_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
