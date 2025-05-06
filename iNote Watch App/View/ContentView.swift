5375dd5305693c26edd42ede06bd9b0db0e8a54e
Rahul choudhary <heyitsmechoudhary@gmail.com>
2025-05-06 14:52:21 +0530

    basic step is complete and plan is already been layout

--- iNote Watch App/View/ContentView.swift
+++ iNote Watch App/View/ContentView.swift
@@ -18,7 +18,24 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 
-                Text("Hello, world")
+                HStack (alignment: .center, spacing: 6){
+                    TextField("Add new Note ", text: $text)
+                    Button{
+                        // 1. Only run the button action code when the text field is not empty
+                        // 2. Create a New note item and intialize it with the text value
+                        // 3. Add the new note item to iNote array (by using Append)
+                        // 4. Make the text field empty
+                        // 5. Save the Note (Functions)
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 42,weight: .semibold))
+                    }
+                    .fixedSize()
+                    .buttonStyle(PlainButtonStyle())
+                    .foregroundColor(.accentColor)
+                    //.buttonStyle(BorderedButtonStyle(tint: .accentColor))
+                }//: HSTACK
+                Spacer()
             }//: VSTACK
             .navigationBarTitle("iNote")
         }
